7ef86ebea122c791f7f6f8d89fd93e7c955a8f21
AnatoliyVinokurov <anatolij.isit@gmail.com>
2023-10-24 16:58:37 +0300

    first commit

---
+++ app/src/main/java/com/anatoliyvinokurov/tictactoe/MainActivity.kt
@@ -0,0 +1,178 @@
+package com.anatoliyvinokurov.tictactoe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.GridLayout
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.*
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+
+//ca-app-pub-3940256099942544/1033173712
+const val AD_UNIT_ID = "ca-app-pub-3940256099942544/1033173712"
+
+class MainActivity : AppCompatActivity() {
+    private var activePlayer = 0
+    private var gameState = IntArray(9) { 2 }
+    private val winningPositions = arrayOf(
+        intArrayOf(0, 1, 2), intArrayOf(3, 4, 5),
+        intArrayOf(6, 7, 8), intArrayOf(0, 3, 6), intArrayOf(1, 4, 7),
+        intArrayOf(2, 5, 8), intArrayOf(0, 4, 8), intArrayOf(2, 4, 6)
+    )
+    private var gameActive = true
+    private var interstitialAd: InterstitialAd? = null
+    private var adIsLoading: Boolean = false
+    private var isSecondGame: Boolean = false
+
+    private lateinit var restart: Button
+    private lateinit var playTheGame: TextView
+    private lateinit var score: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initializeViews()
+        MobileAds.initialize(this) {}
+        restart.setOnClickListener { resetGameAndShowAd() }
+    }
+
+    private fun initializeViews() {
+        restart = findViewById(R.id.button)
+        playTheGame = findViewById(R.id.textView2)
+        score = findViewById(R.id.textView)
+    }
+
+    private fun resetGameAndShowAd() {
+        resetGame()
+        showInterstitialAd()
+    }
+
+    private fun resetGame() {
+        clearBoard()
+        gameState = IntArray(9) { 2 }
+        gameActive = true
+        activePlayer = 0
+        playTheGame.visibility = View.VISIBLE
+        restart.visibility = View.INVISIBLE
+        score.visibility = View.INVISIBLE
+    }
+
+    private fun clearBoard() {
+        val gridLayout = findViewById<GridLayout>(R.id.gridlayout)
+        for (i in 0 until gridLayout.childCount) {
+            val imageView = gridLayout.getChildAt(i) as ImageView
+            imageView.setImageDrawable(null)
+        }
+    }
+
+    fun dropIn(view: View) {
+        val counter = view as ImageView
+        val tappedCounter = counter.tag.toString().toInt()
+        if (gameState[tappedCounter] == 2 && gameActive) {
+            gameState[tappedCounter] = activePlayer
+            counter.translationY = -1500f
+            setCounterImageAndSwitchPlayer(counter)
+            counter.animate().translationYBy(1500f).rotation(36000f).setDuration(300)
+            checkForWin()
+        }
+    }
+
+    private fun setCounterImageAndSwitchPlayer(counter: ImageView) {
+        if (activePlayer == 0) {
+            counter.setImageResource(R.drawable.tictcx)
+            activePlayer = 1
+        } else {
+            counter.setImageResource(R.drawable.tictactoe_o)
+            activePlayer = 0
+        }
+    }
+
+    private fun checkForWin() {
+        var allCellsFilled = true
+        for (i in gameState) {
+            if (i == 2) {
+                allCellsFilled = false
+                break
+            }
+        }
+
+        for (winningPosition in winningPositions) {
+            if (gameState[winningPosition[0]] == gameState[winningPosition[1]] &&
+                gameState[winningPosition[1]] == gameState[winningPosition[2]] &&
+                gameState[winningPosition[1]] != 2
+            ) {
+                gameActive = false
+                val winner = if (activePlayer == 1) "Player 1" else "Player 2"
+                Toast.makeText(this, "$winner Wins", Toast.LENGTH_SHORT).show()
+                playTheGame.visibility = View.GONE
+                score.visibility = View.VISIBLE
+                score.text = "$winner Wins"
+                restart.visibility = View.VISIBLE
+            }
+        }
+
+        if (allCellsFilled && gameActive) {
+            // Все ячейки заполнены, ничья
+            Toast.makeText(this, "It's a Draw", Toast.LENGTH_SHORT).show()
+            restart.visibility = View.VISIBLE
+        }
+    }
+
+    private fun showInterstitialAd() {
+        if (interstitialAd != null) {
+            interstitialAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
+                override fun onAdDismissedFullScreenContent() {
+                    interstitialAd = null
+                    loadAd()
+                }
+
+                override fun onAdFailedToShowFullScreenContent(adError: AdError) {
+                    interstitialAd = null
+                }
+
+                override fun onAdShowedFullScreenContent() {
+                    // Called when ad is dismissed.
+                }
+            }
+            interstitialAd?.show(this)
+        } else {
+            startGame()
+        }
+    }
+
+    private fun loadAd() {
+        val adRequest = AdRequest.Builder().build()
+        InterstitialAd.load(
+            this,
+            AD_UNIT_ID,
+            adRequest,
+            object : InterstitialAdLoadCallback() {
+                override fun onAdFailedToLoad(adError: LoadAdError) {
+                    interstitialAd = null
+                    adIsLoading = false
+                    val error =
+                        "domain: ${adError.domain}, code: ${adError.code}, " + "message: ${adError.message}"
+                }
+
+                override fun onAdLoaded(ad: InterstitialAd) {
+                    interstitialAd = ad
+                    adIsLoading = false
+                }
+            }
+        )
+    }
+
+    private fun startGame() {
+        if (!adIsLoading && interstitialAd == null) {
+            adIsLoading = true
+            loadAd()
+        }
+    }
+}
